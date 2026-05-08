521321a98a67c30107120cd6becf95baf8585ab2
MU-TH-ER <zeerob440@tutanota.com>
2026-05-08 09:11:26 -0400

    update: wht_rbt_obj.js| terminal declared as const

--- wht_rbt_obj.js
+++ wht_rbt_obj.js
@@ -5,7 +5,7 @@
 /*Proudly Engineered by Zachary Roberts, inspired by Dennis Nedry 12 NOV 2025
 "It's the kicker. wht_rbt.obj, whatever it is, it did it all." -Ray Arnold, Jurassic Park*/
 
-let terminal = document.getElementById("terminal")
+const terminal = document.getElementById("terminal");
 
 let greet = "...ACCESSING THE 90's...." +"<br>"+"BUT...";
 
