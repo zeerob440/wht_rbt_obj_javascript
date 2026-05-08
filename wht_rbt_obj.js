bb5f066169a4a021502340d0a6596d4b46633fc6
MU-TH-ER <zeerob440@tutanota.com>
2026-05-08 09:49:15 -0400

    fixed: wht_rbt_obj.js| semantic error

--- wht_rbt_obj.js
+++ wht_rbt_obj.js
@@ -1,6 +1,6 @@
 //imported for zachary_roberts_portfolio, it has been optimized to run in a stand alone environment. 
-/*wht_rbt_obj.js IS NOT AN OBJECT. It is is a cinimatic transition from modern.ccs to retro.ccs that outputs on transition/index.html.
- Named after the notorious Jurassic Park bug "wht_rbt.obj" from Michael Chrichton's Jurassic Park.*/
+/*wht_rbt_obj.js IS NOT AN OBJECT. It is is a cinematic transition from modern.ccs to retro.ccs that outputs on transition/index.html.
+ Named after the notorious Jurassic Park bug "wht_rbt.obj" from Michael Crichton's Jurassic Park.*/
 
 /*Proudly Engineered by Zachary Roberts, inspired by Dennis Nedry 12 NOV 2025
 "It's the kicker. wht_rbt.obj, whatever it is, it did it all." -Ray Arnold, Jurassic Park*/
@@ -35,7 +35,7 @@ let timing = setInterval(() => {
 /*exit loop when iteration is greater than or equal to sentinel value, creates two conditions for the loop to exit if iteration
  exceeds sentinel some reason. returns to root index.*/
 
-if (ITERATION >= SENTINEL) {
+if (iteration >= SENTINEL) {
     clearInterval(timing);
     window.location.href ="terminus/index.html";
     }
